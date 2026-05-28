8393514f80f0f463c68771e8302d4610856f6db4
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-05-28 13:02:16 +0530

    Solved Leetcode 56 Merged Intervals

---
+++ 01_Arrays/56_Merge_Intervals.java
@@ -0,0 +1,20 @@
+// LeetCode 56 - Merge Intervals
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[]> ans=new ArrayList<>();
+        int[] current=intervals[0];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=current[1]){
+                current[1]=Math.max(current[1],intervals[i][1]);
+            }else{
+                ans.add(current);
+                current=intervals[i];
+            }
+        }
+        ans.add(current);
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
