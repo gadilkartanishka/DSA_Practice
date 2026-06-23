40db3151ac176fcd9d0c8d65e04b900d20139923
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-06-23 21:03:37 +0530

    Improved solutions for Leetcode 242

--- 01_Arrays_Hashing/1_Two_Sum.java
+++ 01_Arrays_Hashing/1_Two_Sum.java
@@ -1,16 +1,33 @@
-// LeetCode 1 - Two Sum
+// // LeetCode 1 - Two Sum
+// // Time Complexity: O(n^2)
+// // Space Complexity: O(1)
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+    
+// }
+//OPTIMAL APPROACH
 // Time Complexity: O(n)
-// Space Complexity: O(n)
+//Space Complexity: O(n)
+import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer,Integer> seen=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
+            int difference=target-nums[i];
+            if(seen.containsKey(difference)){
+                return new int[]{seen.get(difference),i};
             }
+            seen.put(nums[i],i);
         }
         return new int[]{};
     }
-    
 }
