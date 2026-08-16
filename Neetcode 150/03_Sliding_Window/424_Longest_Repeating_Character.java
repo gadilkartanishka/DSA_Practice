8f0d8fc63d25a2d52e519becd011487f43951c11
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-08-16 21:53:36 +0530

    Solved Leetcode 424

---
+++ Neetcode 150/03_Sliding_Window/424_Longest_Repeating_Character.java
@@ -0,0 +1,32 @@
+// Leetcode 424 - Longest Repeating Character Replacement
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            int index = s.charAt(right) - 'A';
+            freq[index]++;
+
+            maxFreq = Math.max(maxFreq, freq[index]);
+
+            int windowLength = right - left + 1;
+            int replacements = windowLength - maxFreq;
+
+            if (replacements > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
