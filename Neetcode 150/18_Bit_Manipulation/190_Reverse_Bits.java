46cd2bc1d659381b7b4a1f9ecc10219152a61e59
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-08-19 19:25:18 +0530

    Solved Leetcode 190

---
+++ Neetcode 150/18_Bit_Manipulation/190_Reverse_Bits.java
@@ -0,0 +1,17 @@
+//Leetcode 190- Reverse Bits
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public int reverseBits(int n) {
+        for(int i=0;i<16;i++){
+            int bit1= (n>>i)&1;
+            int bit2=(n>>(31-i))&1;
+
+            if(bit1!=bit2){
+                n=n^(1<<i);
+                n=n^(1<<(31-i));
+            }
+        }
+        return n;
+    }
+}
