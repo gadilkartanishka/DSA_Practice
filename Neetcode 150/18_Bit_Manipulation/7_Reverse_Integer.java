9b9430cb749d955f4fb0c74bc35fb89750cee122
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-08-22 17:50:31 +0530

    Solved leetcode 7

---
+++ Neetcode 150/18_Bit_Manipulation/7_Reverse_Integer.java
@@ -0,0 +1,28 @@
+//Leetcode 7-Reverse Integer
+//Time Complexity : O(1)
+//Space Complexity : O(1)
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+
+        while (x != 0) {
+
+            int digit = x % 10;
+            x = x / 10;
+
+            if (reverse > Integer.MAX_VALUE / 10 ||
+                (reverse == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            if (reverse < Integer.MIN_VALUE / 10 ||
+                (reverse == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            reverse = reverse * 10 + digit;
+        }
+
+        return reverse;
+    }
+}
