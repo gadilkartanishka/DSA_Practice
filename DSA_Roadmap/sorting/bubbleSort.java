6a92f718a111289f21ffafd99bbeb08cba9d16e4
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-09-18 22:00:57 +0530

    Optimise bubble sort

--- DSA_Roadmap/sorting/bubbleSort.java
+++ DSA_Roadmap/sorting/bubbleSort.java
@@ -1,16 +1,25 @@
-//Time complexity: O(n^2)
-//Space complexity: O(1)
+// Time complexity:
+// Best: O(n)
+// Average: O(n^2)
+// Worst: O(n^2)
+
+// Space complexity: O(1)
 class Solution {
     public int[] bubbleSort(int[] nums) {
         int n = nums.length;
 
         for (int i = 0; i < n - 1; i++) {
+            int didSwap = 0;
             for (int j = 0; j < n - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
+                    didSwap = 1;
                 }
+            }
+            if (didSwap == 0) {
+                return nums;
             }
         }
         return nums;
