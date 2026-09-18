e253779b89e8af729cd0fc0cc6af555a3a61642c
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-09-18 21:55:13 +0530

    Bubble sort

---
+++ DSA_Roadmap/sorting/bubbleSort.java
@@ -0,0 +1,18 @@
+//Time complexity: O(n^2)
+//Space complexity: O(1)
+class Solution {
+    public int[] bubbleSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
