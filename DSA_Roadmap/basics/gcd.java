22d8292387983a9eb3e1f0d8aa9c4c416552a018
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-09-22 21:31:00 +0530

    Optimum approach for lcm and gcd

--- DSA_Roadmap/basics/gcd.java
+++ DSA_Roadmap/basics/gcd.java
@@ -1,9 +1,22 @@
+//Brute force
+// class Solution {
+//     public int LCM(int n1, int n2) {
+//         int num = Math.max(n1, n2);
+//         while (num % n1 != 0 || num % n2 != 0) {
+//             num++;
+//         }
+//         return num;
+//     }
+// }
+//OPTIMUM APPROACH
 class Solution {
-    public int LCM(int n1, int n2) {
-        int num = Math.max(n1, n2);
-        while (num % n1 != 0 || num % n2 != 0) {
-            num++;
+    public int GCD(int n1, int n2) {
+        while (n2 != 0) {
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
         }
-        return num;
+
+        return n1;
     }
 }
--- DSA_Roadmap/basics/lcm.java
+++ DSA_Roadmap/basics/lcm.java
@@ -1,10 +1,27 @@
 //brute force approach
+// class Solution {
+//     public int LCM(int n1, int n2) {
+//         int num = Math.max(n1, n2);
+//         while (num % n1 != 0 || num % n2 != 0) {
+//             num++;
+//         }
+//         return num;
+//     }
+// }
 class Solution {
-    public int LCM(int n1, int n2) {
-        int num = Math.max(n1, n2);
-        while (num % n1 != 0 || num % n2 != 0) {
-            num++;
+    public int GCD(int n1, int n2) {
+        while (n2 != 0) {
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
         }
-        return num;
+
+        return n1;
+    }
+
+    public int LCM(int n1, int n2) {
+        int gcd = GCD(n1, n2);
+
+        return (n1 / gcd) * n2;
     }
 }
